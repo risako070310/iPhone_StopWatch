61405aae27544dc76d405514a380f8f169a324c5
risako070310 <risako070310@gmail.com>
2022-06-10 16:56:27 +0900

    add hantei

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -11,19 +11,37 @@ class ViewController: UIViewController {
     
     @IBOutlet var timeLabel: UILabel!
     
+    @IBOutlet var hanteiLabel: UILabel!
+    
     var count: Float = 0
     
     var timer: Timer = Timer()
     
+    var hanteiText: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        hanteiLabel.text = hanteiText
     }
     
     @objc func up(){
         count = count + 0.01
         
         timeLabel.text = String(format: "%.2f", count)
+    }
+    
+    @objc func hantei(){
+        if count <= 10.2 && count >= 9.8 {
+            hanteiText = "PERFECT!!"
+        } else if count <= 10.3 && count >= 9.7 {
+            hanteiText = "GREAT!!"
+        } else if count <= 10.5 && count >= 9.5 {
+            hanteiText = "GOOD!!"
+        }
+        
+        hanteiLabel.text = hanteiText
     }
     
     @IBAction func start(){
@@ -42,11 +60,15 @@ class ViewController: UIViewController {
         if timer.isValid{
             timer.invalidate()
         }
+        
+        self.hantei()
     }
     
     @IBAction func reset(){
         count = 0
         timeLabel.text = "0.0"
+        hanteiText = ""
+        hanteiLabel.text = hanteiText
         
         if timer.isValid{
             timer.invalidate()
